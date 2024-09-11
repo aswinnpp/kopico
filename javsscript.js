f0faf16245640e5553d7199b718bc491aa2ffad1
Aswin Cp <aswincp554@gmail.com>
2024-09-11 17:10:30 +0530

    f project

---
+++ javsscript.js
@@ -0,0 +1,12 @@
+
+function icon1() {
+    alert('press ok to go facebook');
+}function icon2() {
+    alert('press ok to go instagram');
+}
+function icon3() {
+    alert('press ok to go linkedin');
+}
+function icon4() {
+    alert('press ok to go youtube');
+}
